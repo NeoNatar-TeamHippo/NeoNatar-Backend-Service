e240d85138d9b506ac5c179ab0bdcea504f882fb
codemutatio <faithomojola@gmail.com>
2019-12-07 21:33:27 +0100

    fixed route

--- functions/src/routes/campaign.js
+++ functions/src/routes/campaign.js
@@ -7,7 +7,7 @@ const router = express.Router();
 router.post('/', FBauth, campaign.create);
 router.get('/', FBauth, campaign.getAll);
 router.get('/:id', FBauth, campaign.getOne);
-router.put('/:id', FBauth, campaign.updateSavedLocation);
-router.delete('/:id', FBauth, campaign.deleteSavedLocation);
+router.put('/:id', FBauth, campaign.updateCampaign);
+router.delete('/:id', FBauth, campaign.deleteCampaign);
 
 module.exports = router;
