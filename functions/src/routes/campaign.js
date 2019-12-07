5c044c84fc62ab18d3454d0d5591330f9230ddd8
codemutatio <faithomojola@gmail.com>
2019-12-07 11:33:05 +0100

    minor change

--- functions/src/routes/campaign.js
+++ functions/src/routes/campaign.js
@@ -6,7 +6,7 @@ const router = express.Router();
 
 router.post('/', FBauth, campaign.create);
 router.get('/', FBauth, campaign.getAll);
-router.get('/:id', FBauth, scampaign.getOne);
+router.get('/:id', FBauth, campaign.getOne);
 router.put('/:id', FBauth, campaign.updateSavedLocation);
 router.delete('/:id', FBauth, campaign.deleteSavedLocation);
 
